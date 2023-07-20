0fb3b4c5639638d8f1b18622d01c2a4c870d02eb
anacirqueira <cirqueiraana6@gmail.com>
2023-07-20 18:04:54 -0300

    Aplicando a lógica

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// Função que captura o número digitado e o guarda
+const randomNumber = Math.round(Math.random() * 10)
+let xTentativas = 1
 
 function handleClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
 
-  console.log(inputNumber.value)
+  if (Number(inputNumber.value) == randomNumber) {
+    document.querySelector(".screen1").classList.add("hide")
+    document.querySelector(".screen2").classList.remove("hide")
+
+    document.querySelector(".screen2 h2").innerText = 
+    `Você acertou em ${xTentativas} tentativas`
+  }
+
+  xTentativas++
 }
