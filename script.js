1feb032b66d89cace00afc146e1f4fdefea07fff
anacirqueira <cirqueiraana6@gmail.com>
2023-07-25 14:02:21 -0300

    Aplicando eventos e callbacks

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
 const randomNumber = Math.round(Math.random() * 10)
 let xTentativas = 1
 
-function handleClick(event) {
-  event.preventDefault()
+// Função Callback - Será chamada de volta em outra func
+
+function handleTryClick(event) {
+  event.preventDefault() // não faça o padrão (não envie o form)
 
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
 
     document.querySelector(".screen2 h2").innerText = 
     `Você acertou em ${xTentativas} tentativas`
   }
 
+  inputNumber.value = ""
   xTentativas++
 }
 
-const button = document.querySelector('button')
 
-button.addEventListener('click', function {
+// Eventos - Clique
 
-  document.querySelector('')
+const btnTry = document.querySelector("#btnTry")
+const btnAgain = document.querySelector("#btnAgain")
+
+btnTry.addEventListener('click', handleTryClick)
+
+btnAgain.addEventListener('click', function () {
+
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+
+  xTentativas = 1
+  
 })
